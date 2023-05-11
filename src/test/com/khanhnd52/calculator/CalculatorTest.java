af7dd5b2365a9263234596000ac948623bf04b33
khanhnd52 <khanh.nd5252@gmail.com>
2023-05-12 03:35:00 +0700

    simple test

---
+++ src/test/com/khanhnd52/calculator/CalculatorTest.java
@@ -0,0 +1,34 @@
+package com.khanhnd52.calculator;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+
+    private Calculator calc;
+
+    @BeforeEach
+    void setUp() {
+        calc = new Calculator();
+    }
+
+    @Test
+    public void canAddZeroPlusZero() {
+        int sum = calc.add(0, 0);
+        assertEquals(0, sum, "Was expecting sum of 0");
+    }
+
+    @Test
+    public void canAddOnePlusOne() {
+        int sum = calc.add(1, 1);
+        assertEquals(2, sum);
+    }
+
+    @Test
+    public void canAddWithNegativeNumber() {
+        int sum = calc.add(-1, 1);
+        assertEquals(0, sum);
+    }
+}
